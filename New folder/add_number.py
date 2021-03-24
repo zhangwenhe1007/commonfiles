bb8014e06f00d861db1ae764da88f37c99d12beb
zhangwenhe1007 <78190776+zhangwenhe1007@users.noreply.github.com>
2021-03-24 17:52:04 -0400

    Add files via upload

---
+++ New folder/add_number.py
@@ -0,0 +1,13 @@
+from csv import writer
+
+def append_list_as_row(file_name, list_of_elem):
+  with open(file_name, 'a+', newline='') as write_obj:
+
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
+
+def add_num(number):
+    append_list_as_row('unwanted_calls.csv', [number])
