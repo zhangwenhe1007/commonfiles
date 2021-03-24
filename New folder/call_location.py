7d042708d23417d9d14a3636fdf46316c46768af
zhangwenhe1007 <78190776+zhangwenhe1007@users.noreply.github.com>
2021-03-24 17:54:30 -0400

    Update call_location.py

--- New folder/call_location.py
+++ New folder/call_location.py
@@ -1,4 +1,3 @@
-
 def call_location(number):
     import phonenumbers
     from phonenumbers import geocoder
@@ -13,7 +12,7 @@ def call_location(number):
         x = phonenumbers.parse(num, None)
         location = geocoder.description_for_number(x, 'en')
     except phonenumbers.phonenumberutil.NumberParseException:
-        return 'Invalid number'
+        return 'Invalid number.'
 
     if '+' in number:
         number = number.replace('+', '')
@@ -52,13 +51,13 @@ def call_location(number):
         # this means the number comes from canada or the u.s.
         if number1[1:4] == ['5', '1', '4'] or number1[1:4] == ['4', '3', '8']:
             # this means the number is from montreal
-            return number + " is from Montréal."
+            return number + " is from Montraal." + "#123"
         if number1[1:4] == ["3", "5", "4"] or number1[1:4] == ['4', '5', '0'] or number1[1:4] == ['5', '7', '9']:
-            return number + " is from Banlieues de Montréal."
+            return number + " is from Montreal Suburbs." + "#123"
         if number1[1:4] == ['4', '1', '8'] or number1[1:4] == ['5', '8', '1'] or number1[1:4] == ['3', '6', '7']:
-            return number + " is from Québec Est."
+            return number + " is from Quebec East." + "#123"
         if number1[1:4] == ['8', '1', '9'] or number1[1:4] == ['8', '7', '3']:
             # this means that the number is from quebec
-            return number + " is from Québec Nord or Ouest."
+            return number + " is from Quebec North or West." + "#123"
     else:
-        return number + " is from " + location
+        return number + " is from " + location + "#123"
